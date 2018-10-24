921cb5fc1a74b1cbfcdbd41b841c47a849520af9
lzz <liuzizhang1994@gmail.com>
2018-10-24 17:22:05 +0800

    完成algorithms.leetcode.RemoveDuplicatesFromsortedArray

--- src/algorithms/leetcode/RemoveDuplicatesFromSortedArray.java
+++ src/algorithms/leetcode/RemoveDuplicatesFromSortedArray.java
@@ -2,4 +2,13 @@ package algorithms.leetcode;
 
 public class RemoveDuplicatesFromSortedArray {
 
+    //length<2  i-1
+    public static int solution(int[] nums) {
+        if (nums.length < 2) return nums.length;
+        int newlen = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) nums[newlen++] = nums[i];
+        }
+        return newlen;
+    }
 }
---
+++ test/datastructure/leetcode/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,17 @@
+package datastructure.leetcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static algorithms.leetcode.RemoveDuplicatesFromSortedArray.solution;
+
+
+public class RemoveDuplicatesFromSortedArray {
+
+    @Test
+    public void solutionTest() {
+        int[] nums = new int[]{1, 1, 2};
+        int actual = solution(nums);
+        Assert.assertEquals(2, actual);
+    }
+}
