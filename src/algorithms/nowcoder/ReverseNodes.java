d8c42db017171d9f1d5c37b47f319b0228138979
lzz <liuzizhang1994@gmail.com>
2019-06-16 22:55:03 +0800

    finish algorithms.nowcoder.ReverseNodes

---
+++ src/algorithms/nowcoder/ReverseNodes.java
@@ -0,0 +1,28 @@
+package algorithms.nowcoder;
+
+//反转单链表，基本操作
+public class ReverseNodes {
+
+    public class Node {
+        int value;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public static Node solution(Node head) {
+        Node pre = null;
+        Node next = null;
+
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+
+        return pre;
+    }
+}
